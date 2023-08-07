b158622e4962c70e6b0c980429e18b34a0960fa1
Rizky Saskia Putra <36327242+rizkysaskiaputra@users.noreply.github.com>
2023-08-08 00:27:43 +0700

    created api samples

--- demo.js
+++ demo.js
@@ -1,2 +1,340 @@
 console.log("demo");
 console.log("pull request");
+
+const api = {
+  kind: "blogger#postList",
+  nextPageToken: "CgkIChikj86KmzEQhpmAlI32gYI7GAA",
+  items: [
+    {
+      kind: "blogger#post",
+      id: "7305247229428072954",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T07:07:00-07:00",
+      updated: "2023-08-01T07:07:12-07:00",
+      url: "http://rspux.blogspot.com/2023/08/how-to-appreciate-the-beauty-and-diversity-of-nature.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/7305247229428072954",
+      title: "How to Appreciate the Beauty and Diversity of Nature",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiFGHtby1px7ZKtNUSQr6vy7k8gRTpsEtgvUSwY8JkVUC7DhaFu96KhLpybQm9n8rYQ96SrtUljFYsDKXtHHsOHob_kwuTXKDEURni9WIZGC7QylZAlGcTeZlSXLJyjbPtsOdoGUdq3l3l-xWvWF16gIAunW49wiob8-Ui1vtDdfNc61hlLeiNZL5LNH5Y/s400/ux12.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/7305247229428072954/comments",
+      },
+      labels: ["nature"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk4ODMyODEwCm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "593375509841236676",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T07:02:00-07:00",
+      updated: "2023-08-01T07:02:59-07:00",
+      url: "http://rspux.blogspot.com/2023/08/10-amazing-facts-about-nature-you-didnt-know.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/593375509841236676",
+      title: "10 Amazing Facts About Nature You Didn't Know",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhoy-mn51rSo7j0xtjnaYDBsgmdLXM_gdrnBIsdBRtjDabf5cabZT_Z8mzOT6BB7O6sMJRsaSbvQWtaP5UF152PUb1-KR2CJeyMIyq6gvKT73EBz3MYRGL9VcfJh1eAC9ri6tkAoszqvBd5QYm9VK0N2yDnDJFu6hgZCyGVSpHrZ8a11twmZwAU9cx3RLE/s400/ux11.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/593375509841236676/comments",
+      },
+      labels: ["nature"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk4NTc5MzY4Cm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "7031717732551108165",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T06:57:00-07:00",
+      updated: "2023-08-01T06:57:51-07:00",
+      url: "http://rspux.blogspot.com/2023/08/how-to-use-nature-as-source-of-inspiration-and-creativity.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/7031717732551108165",
+      title: "How to Use Nature as a Source of Inspiration and Creativity",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgkTjCrxSoocslglxrRk9eucFiTz1Iza1aRq9YEQK67m4Qcb6FxoqGHKxF7YuEdp087krEHFcEzRosGJ9d9qMpCJqC1s1tz-C6-hqY1WQUxk0L1ntFxUu1iWYBE9MZoxZV5w4rdx--j31HP1og5EOhKH7TCuh28IziyiC4u48p_eF7om8AcHZCUD685jys/s400/ux10.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/7031717732551108165/comments",
+      },
+      labels: ["nature"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk4MjcxMDk0Cm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "8143908000901626419",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T06:51:00-07:00",
+      updated: "2023-08-01T06:51:24-07:00",
+      url: "http://rspux.blogspot.com/2023/08/how-to-find-your-zen-at-beach.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/8143908000901626419",
+      title: "How to Find Your Zen at the Beach",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhzvGHopcoivqxh6tIOJnLdlTgCjqPstbfibQmoq4OKF1cx34nz4Nq0vgupCbB9F4XK-O740kYDvNA2KG-OMM0xbZt1Y1ko6gI4rH2cTAILButN4y_FON-r5Khs6KyVr0yiRkXn09qsDaDtoTUmPd_WeloKlynztKIRYfpdYHwSAl0_hDmPcb19IULlby8/s400/ux9.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/8143908000901626419/comments",
+      },
+      labels: ["beach"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk3ODg0NDYyCm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "466195495058229359",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T06:45:00-07:00",
+      updated: "2023-08-01T06:45:53-07:00",
+      url: "http://rspux.blogspot.com/2023/08/the-history-and-culture-of-beach-life.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/466195495058229359",
+      title: "The History and Culture of Beach Life",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjhEDJzeVDht7AFZbkAvWcybLG4NfioR4FtanD2cv6volVFvITSdZ6KSMFSJrxDJ_BZHPPg4ScwvzNf23pcyeZDlYNCzTJnlIFVxlKHlXCOq9nCasaq-U6PrFhq5-2blF5pf1cPDEtzRKZte5xAFpPtzP3VvQ459-U1O_SEC90Cxg6TBDX3I5PtqArE-pw/s400/ux8.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/466195495058229359/comments",
+      },
+      labels: ["beach"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk3NTUzNTgxCm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "3151766084244200995",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T06:45:00-07:00",
+      updated: "2023-08-01T06:45:17-07:00",
+      url: "http://rspux.blogspot.com/2023/08/10-best-beach-destinations-in-the-world.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/3151766084244200995",
+      title: "10 Best Beach Destinations in the World",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi3tOm9mDBcdsywvyBbr0gdrzaNNNVznpALSEnEzdFfPUSdo_AxossgJIlTH2oV8L_DypvyKxsgtazDNoRVDQrto2m3d7DDIxjX7zIOxNZiel067TTjd-8kJcC_vK5LmKe83W4pCRgfBmWQScUoAjI7jV1oggrPQbZDkzuSs37zirmL8ash9M6JcOMjSCo/s400/ux7.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/3151766084244200995/comments",
+      },
+      labels: ["beach"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk3NTE3NjU4Cm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "4720167231030647241",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T06:44:00-07:00",
+      updated: "2023-08-01T06:44:09-07:00",
+      url: "http://rspux.blogspot.com/2023/08/how-to-protect-and-preserve-the-beach-environment.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/4720167231030647241",
+      title: "How to Protect and Preserve the Beach Environment",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgQk5socL8sssN6pNhJGBRuaAIAkM4iX5HdlBjNwLLIzxCjRdIb9wwuSh61ejl7feE2wcmsQLc-1peWnTiZXAVE0o7-9w5yJw9wxzFO77rwTEJXmoCPVfLfxEmCMSWFT94XDnq4sOJXSG7ZFNjjdHbRdkUkxiULDKEvrxUjohkdfl4H3pj64SLLxe7hYic/s400/ux6.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/4720167231030647241/comments",
+      },
+      labels: ["nature"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk3NDQ5NjkyCm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "5571039335971092232",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T06:43:00-07:00",
+      updated: "2023-08-01T06:43:16-07:00",
+      url: "http://rspux.blogspot.com/2023/08/how-to-enjoy-the-beach-safely-and-responsibly.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/5571039335971092232",
+      title: "How to Enjoy the Beach Safely and Rensponsibly",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjLiLAooYbaUoDmNlpBrWXqy5ALcgGNtatNl5A7HA9RavjJOgZKWTvQNZOkmoIh7JkyE83tpM46HiWQ7eo_X6BCVdVQwUt5kXOCSTeLMeaolpX7EPQBhqpb1J8nujUYZLpRdWWkVNoP8O5iXA0NyQf9Ux14TSGGMVJRz_Qn2RfhSluRt4v5SV8zYgaHXMY/s400/ux5.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/5571039335971092232/comments",
+      },
+      labels: ["nature"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk3Mzk2NTUwCm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "3594137877398789935",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T06:42:00-07:00",
+      updated: "2023-08-01T06:42:24-07:00",
+      url: "http://rspux.blogspot.com/2023/08/the-importance-of-protecting-and-conserving-nature.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/3594137877398789935",
+      title: "The Importance of Protecting and Conserving Nature",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjgAmUCX8WwOsRsi0dYSlreZFI0v4oXWcZaykc9M-czvFu6p14BW5BCr6VoeGMXVlqVBHbJmSn487X4VeYrwvCQxp5Ok9PeaMqT9sNLKhw3MxUhptGofuNnOcpNwY_ohdEjS4c98kY7Rn24yKEiBNpcEbENNY2kaZnIWuaZpt4HjoXeCL5Jh6ky2kNyj2I/s400/ux4.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/3594137877398789935/comments",
+      },
+      labels: ["nature"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk3MzQ0MDcyCm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+    {
+      kind: "blogger#post",
+      id: "1755237829538153892",
+      blog: {
+        id: "4252532404171836550",
+      },
+      published: "2023-08-01T06:39:00-07:00",
+      updated: "2023-08-01T06:39:48-07:00",
+      url: "http://rspux.blogspot.com/2023/08/how-to-life-more-sustainably-and-eco-friendly.html",
+      selfLink:
+        "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/1755237829538153892",
+      title: "How to Life More Sustainably and Eco-Friendly",
+      images: [
+        {
+          url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjd1dCnocfQ1cvaQfL6Hpq1jkkqXrw2tjpGUWDW7GDYTyXgpUoSwdS5ZORpyxXgKZZiqiPQFqowzbuDMz0_W8qbsWnUNf75bQ0xy-lkyIdPxtuLONYhM3BAeOZv3fgMRRjGxZK4UVr2AvgilFUPZvJH8LRMav2GW9v1NtVY3fKpl6O65lxgV1-0cg_NsTM/s400/ux3.jpg",
+        },
+      ],
+      author: {
+        id: "01883940573886924473",
+        displayName: "Rizky S.P",
+        url: "https://www.blogger.com/profile/01883940573886924473",
+        image: {
+          url: "//2.bp.blogspot.com/-KJgMQYLgDHs/ZMf8tKacdyI/AAAAAAAAADs/V6FiHdwg5A0DW0EPl32AiL91ICB2OV7bwCK4BGAYYCw/s35/kia.png",
+        },
+      },
+      replies: {
+        totalItems: "0",
+        selfLink:
+          "https://www.googleapis.com/blogger/v3/blogs/4252532404171836550/posts/1755237829538153892/comments",
+      },
+      labels: ["city"],
+      etag: '"dGltZXN0YW1wOiAxNjkwODk3MTg4NzcyCm9mZnNldDogLTI1MjAwMDAwCg"',
+    },
+  ],
+  etag: '"MjAyMy0wOC0wMVQxNDoxMDo0Ni44ODNa"',
+};
